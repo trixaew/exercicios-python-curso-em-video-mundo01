9bdcd7d8943c956cd91457b935efa30524b3982c
trix <96706256+trixaew@users.noreply.github.com>
2026-08-31 20:14:56 -0300

    Add welcome message program in ex002.py

---
+++ ex002.py
@@ -0,0 +1,6 @@
+"""
+Desafio 002
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+"""
+nome = str(input("Qual é o seu nome? "))
+print(f"É um prazer te conhecer {nome}! Seja bem vindo(a)!")
