4989db690bef616b7486b6b557393026805e2e57
trix <96706256+trixaew@users.noreply.github.com>
2026-09-02 17:34:44 -0300

    Add meter conversion program
    Implement program to convert meters to centimeters and millimeters.

---
+++ ex008.py
@@ -0,0 +1,6 @@
+"""
+Exercício Python 8: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+"""
+
+metros = float(input("Informe o valor em metros: "))
+print(f"O valor em centimetros é {metros*100} e o valor em milimetros {metros*1000}")
