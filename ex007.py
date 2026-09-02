b4aed1ccabc567a495b3f1b04988094eaede5932
trix <96706256+trixaew@users.noreply.github.com>
2026-09-02 17:33:29 -0300

    Add program to calculate student average score

---
+++ ex007.py
@@ -0,0 +1,7 @@
+"""
+Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+"""
+
+nota = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+print(f"a media é: {(nota + nota2)/2}")
