c999814ecbf7baaae5b128a52ebaa2658df514ea
trix <96706256+trixaew@users.noreply.github.com>
2026-09-01 21:52:17 -0300

    Add program to sum two numbers

---
+++ ex003.py
@@ -0,0 +1,8 @@
+"""
+Desafio 003
+Crie um programa que leia dois números e mostre a soma entre eles.
+"""
+
+num1 = float(input("Primeiro numero: "))
+num2 = float(input("Segundo numero: "))
+print(f"A soma entre os numeros {num1} + {num2} = {num1 + num2}")
