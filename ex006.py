31747c77cb42bf4c68b54b30c1080ca0ab13c11f
trix <96706256+trixaew@users.noreply.github.com>
2026-09-02 17:32:44 -0300

    Add algorithm to calculate double, triple, and square root

---
+++ ex006.py
@@ -0,0 +1,5 @@
+"""
+Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+"""
+num = int(input("Digite um numero: "))
+print(f"O dobro desse numero é {num * 2}, o triplo {num * 3} e a raiz quadrada {num**(1/2):.2f}")
