d8873ce2758fd1e89e9aef8d4dbf03ac5c2b2f23
trix <96706256+trixaew@users.noreply.github.com>
2026-09-04 20:01:14 -0300

    Implement multiplication table program in Python

---
+++ ex009.py
@@ -0,0 +1,15 @@
+"""
+Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+"""
+
+num = int(input("Digite um numero inteiro: "))
+print(f"{num} X {1} = {num*1}")
+print(f"{num} X {2} = {num*2}")
+print(f"{num} X {3} = {num*3}")
+print(f"{num} X {4} = {num*4}")
+print(f"{num} X {5} = {num*5}")
+print(f"{num} X {6} = {num*6}")
+print(f"{num} X {7} = {num*7}")
+print(f"{num} X {8} = {num*8}")
+print(f"{num} X {9} = {num*9}")
+print(f"{num} X {10} = {num*10}")
